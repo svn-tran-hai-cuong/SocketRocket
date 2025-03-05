e5ff21655b6b90bf99e1db8ea5aa83649f2c11f8
tran.hai.cuong <tran.hai.cuong@sun-asterisk.com>
2025-03-05 15:24:34 +0700

    Revert "test"
    This reverts commit fee87ee10eef4e80954b14ad231a68284a61c100.

--- Package.swift
+++ Package.swift
@@ -18,11 +18,10 @@ let package = Package(
     targets: [
         .target(
             name: "SocketRocket",
-            path: ".",
+            path: "SocketRocket",
             sources: ["SocketRocket.h","SRSecurityPolicy.h","SRSecurityPolicy.m","SRWebSocket.h","SRWebSocket.m","NSURLRequest+SRWebSocket.h","NSURLRequest+SRWebSocket.m","NSRunLoop+SRWebSocket.h","NSRunLoop+SRWebSocket.m","SRError.h","SRError.m","SRHash.h","SRHash.m","SRHTTPConnectMessage.h","SRHTTPConnectMessage.m","SRLog.h","SRLog.m","SRMutex.h","SRMutex.m","SRPinningSecurityPolicy.h","SRPinningSecurityPolicy.m","SRRandom.h","SRRandom.m","SRSIMDHelpers.h","SRSIMDHelpers.m","SRURLUtilities.h","SRURLUtilities.m","NSURLRequest+SRWebSocketPrivate.h","SRRunLoopThread.h","SRRunLoopThread.m","SRConstants.h","SRProxyConnect.h","SRProxyConnect.m","SRConstants.m","SRProxyConnect.h","SRProxyConnect.m","SRConstants.m","SRDelegateController.h","SRDelegateController.m","SRIOConsumer.h","SRIOConsumer.m","SRIOConsumerPool.h","SRIOConsumerPool.m"],
             cSettings: [
                 .headerSearchPath("Internal/**"),
             ]),
-            publicHeadersPath: "include",
     ]
 )
