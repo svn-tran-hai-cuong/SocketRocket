f1a3e21a96e1788f6238df94d8be06311a67ccd0
tran.hai.cuong <tran.hai.cuong@sun-asterisk.com>
2025-03-07 11:17:22 +0700

    a

--- Package.swift
+++ Package.swift
@@ -1,27 +1,27 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "SocketRocket",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9),
+        .iOS(.v11)
     ],
     products: [
         .library(
             name: "SocketRocket",
-            type: .dynamic,
-            targets: ["SocketRocket"]),
+            targets: ["SocketRocket"]
+        ),
     ],
-    dependencies: [],
     targets: [
         .target(
             name: "SocketRocket",
             path: "SocketRocket",
-            sources: ["SocketRocket.h","SRSecurityPolicy.h","SRSecurityPolicy.m","SRWebSocket.h","SRWebSocket.m","NSURLRequest+SRWebSocket.h","NSURLRequest+SRWebSocket.m","NSRunLoop+SRWebSocket.h","NSRunLoop+SRWebSocket.m","SRError.h","SRError.m","SRHash.h","SRHash.m","SRHTTPConnectMessage.h","SRHTTPConnectMessage.m","SRLog.h","SRLog.m","SRMutex.h","SRMutex.m","SRPinningSecurityPolicy.h","SRPinningSecurityPolicy.m","SRRandom.h","SRRandom.m","SRSIMDHelpers.h","SRSIMDHelpers.m","SRURLUtilities.h","SRURLUtilities.m","NSURLRequest+SRWebSocketPrivate.h","SRRunLoopThread.h","SRRunLoopThread.m","SRConstants.h","SRProxyConnect.h","SRProxyConnect.m","SRConstants.m","SRProxyConnect.h","SRProxyConnect.m","SRConstants.m","SRDelegateController.h","SRDelegateController.m","SRIOConsumer.h","SRIOConsumer.m","SRIOConsumerPool.h","SRIOConsumerPool.m"],
+            exclude: ["Info.plist"], // Loại bỏ file không cần thiết
+            publicHeadersPath: "include",
             cSettings: [
-                .headerSearchPath("Internal/**"),
-            ]),
+                .headerSearchPath("."),
+                .define("SR_LOG_ENABLED", to: "1")
+            ]
+        )
     ]
 )
