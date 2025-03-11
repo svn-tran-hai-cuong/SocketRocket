c67ec82b554abd1abc96d730b6a39e81782ee6dc
tran.hai.cuong <tran.hai.cuong@sun-asterisk.com>
2025-03-11 08:03:51 +0700

    draft

--- Package.swift
+++ Package.swift
@@ -16,27 +16,6 @@ let package = Package(
         .target(
             name: "SocketRocket",
             path: "SocketRocket",
-            sources: [
-                "NSRunLoop+SRWebSocket.m",
-                "NSURLRequest+SRWebSocket.m",
-                "SRConstants.m",
-                "SRDelegateController.m",
-                "SRError.m",
-                "SRHash.m",
-                "SRHTTPConnectMessage.m",
-                "SRIOConsumer.m",
-                "SRIOConsumerPool.m",
-                "SRLog.m",
-                "SRMutex.m",
-                "SRPinningSecurityPolicy.m",
-                "SRProxyConnect.m",
-                "SRRandom.m",
-                "SRRunLoopThread.m",
-                "SRSecurityPolicy.m",
-                "SRSIMDHelpers.m",
-                "SRURLUtilities.m",
-                "SRWebSocket.m"],
-            publicHeadersPath: "include",
             cSettings: [
                 .headerSearchPath("include")
             ]
