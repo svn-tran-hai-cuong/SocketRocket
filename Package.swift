3af7386920b7341248435aefcbba29bfa53cbfad
tran.hai.cuong <tran.hai.cuong@sun-asterisk.com>
2025-03-11 08:47:52 +0700

    draft

--- Package.swift
+++ Package.swift
@@ -4,22 +4,35 @@ import PackageDescription
 let package = Package(
     name: "SocketRocket",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v11),
+        .macOS(.v10_13),
+        .tvOS(.v11)
     ],
     products: [
         .library(
             name: "SocketRocket",
-            type: .static,
             targets: ["SocketRocket"]
-        ),
+        )
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "SocketRocket",
+            dependencies: [],
             path: "SocketRocket",
+            exclude: [],
+            publicHeadersPath: "include",
             cSettings: [
-                .headerSearchPath("include")
+                .headerSearchPath("include"),
+                .define("USE_ICUCORE", to: "1")
+            ],
+            linkerSettings: [
+                .linkedFramework("CFNetwork", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Security"),
+                .linkedFramework("CoreServices", .when(platforms: [.macOS])),
+                .linkedLibrary("icucore")
             ]
         )
     ]
 )
+
